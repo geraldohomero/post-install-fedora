493bbcf4de3d341dc674e5cfb97957657ed43694
Geraldo Homero <geraldohomero@protonmail.com>
2025-01-03 10:32:32 -0300

    Create update.sh

---
+++ update.sh
@@ -0,0 +1,26 @@
+RED='\e[1;91m'
+GREEN='\e[1;92m'
+BLUE='\e[1;94m'
+ORANGE='\e[1;93m'
+NO_COLOR='\e[0m'
+
+check_internet() {
+  if ! ping -c 1 8.8.8.8 -q &> /dev/null; then
+    echo -e "${RED}[ERROR] - Your computer does not have an Internet connection.${NO_COLOR}"
+    exit 1
+  else
+    echo -e "${GREEN}[INFO] - Internet connection verified.${NO_COLOR}"
+  fi
+}
+
+upgrade_cleanup () {
+  echo -e "${GREEN}[INFO] - Performing upgrade and cleanup...${NO_COLOR}"
+  sleep 1
+  sudo dnf check
+  sudo dnf upgrade -y
+  sudo dnf autoremove -y
+  flatpak update -y
+}
+
+check_internet
+upgrade_cleanup 
